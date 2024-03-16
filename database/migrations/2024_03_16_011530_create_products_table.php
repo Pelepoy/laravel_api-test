c5f530acdac98e613a80856f0ff4a56b657f6b6e
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-03-16 10:19:45 +0800

    Add user_id foreign key and product attributes to products table

--- database/migrations/2024_03_16_011530_create_products_table.php
+++ database/migrations/2024_03_16_011530_create_products_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('product_name');
+            $table->text('product_description');
+            $table->decimal('product_price', 8, 2);
+            $table->json('product_tag');
             $table->timestamps();
         });
     }
