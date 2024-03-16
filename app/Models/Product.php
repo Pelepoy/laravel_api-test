30b708282e964e77e881ea4c20c5e5f683a6987a
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-03-16 10:15:49 +0800

    Add fillable attributes and static arrays to Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,44 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'product_name',
+        'product_description',
+        'product_price',
+        'product_tag',
+    ];
+
+    public static array $product_name = [
+        'iPhone',
+        'Make Up',
+        'Charger',
+        'Corndog',
+        '48 Laws of Power',
+        'Polo Shirt',
+        'Electric Fan',
+        'Laptop',
+        'Dinner Set',
+        'Remote Control Car',
+    ];
+
+    public static array $product_tag = [
+        'Electronics',
+        'Fashion',
+        'Home',
+        'Kitchen',
+        'Beauty',
+        'Toys',
+        'Food',
+        'Books',
+    ];
+
+    protected $casts = [
+        'product_tag' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
